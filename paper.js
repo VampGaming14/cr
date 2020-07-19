5b9ad9c83cbdf743e1d9e529eb626de546d08c4f
VampGaming14 <65399588+VampGaming14@users.noreply.github.com>
2020-07-19 18:39:11 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,40 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+			this.image=loadImage("paper.png")
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var paperpos=this.body.position;		
+			image (this.image,0,0, this.r, this.r);
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+	image(){
+		this.image=loadImage("paper.png")
+
+	}
+
+}
